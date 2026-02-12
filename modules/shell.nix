d590393724442c137c06256454e54da78501aff7
lks-hrsch <lkshrsch2001@outlook.com>
2026-02-12 15:57:23 +0100

    fix: indentation

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,49 +1,51 @@
 { inputs, ... }:
 {
-  flake.nixosModules.shell = { pkgs, ... }: {
-    environment.shells = with pkgs; [ zsh ];
-    users.defaultUserShell = pkgs.zsh;
-    programs.zsh.enable = true;
-  };
+  flake = {
+    nixosModules.shell = { pkgs, ... }: {
+      environment.shells = with pkgs; [ zsh ];
+      users.defaultUserShell = pkgs.zsh;
+      programs.zsh.enable = true;
+    };
 
-  flake.darwinModules.shell = { pkgs, ... }: {
-    environment.shells = with pkgs; [ zsh ];
-    programs.zsh.enable = true;
-  };
+    darwinModules.shell = { pkgs, ... }: {
+      environment.shells = with pkgs; [ zsh ];
+      programs.zsh.enable = true;
+    };
 
-  flake.homeManagerModules.shell = { pkgs, ... }: {
-    programs.zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-      history = {
-        expireDuplicatesFirst = true;
-        ignoreDups = true;
+    homeManagerModules.shell = { pkgs, ... }: {
+      programs.zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        enableCompletion = true;
+        history = {
+          expireDuplicatesFirst = true;
+          ignoreDups = true;
+        };
       };
-    };
 
-    programs.starship = {
-      enable = true;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-      settings = {
-        hostname.ssh_only = false;
-        username.show_always = true;
+      programs.starship = {
+        enable = true;
+        enableZshIntegration = true;
+        enableBashIntegration = true;
+        settings = {
+          hostname.ssh_only = false;
+          username.show_always = true;
+        };
       };
-    };
 
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
 
-    programs.tmux = {
-      enable = true;
-      clock24 = true;
-    };
+      programs.tmux = {
+        enable = true;
+        clock24 = true;
+      };
 
-    programs.fastfetch.enable = true;
-    programs.btop.enable = true;
+      programs.fastfetch.enable = true;
+      programs.btop.enable = true;
+    };
   };
 }
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,35 +1,37 @@
 { inputs, ... }:
 {
-  flake.nixosModules.ssh = { config, ... }: {
-    services.openssh = {
-      enable = true;
-      authorizedKeysFiles = [ config.sops.secrets."ssh-public-key".path ];
+  flake = {
+    nixosModules.ssh = { config, ... }: {
+      services.openssh = {
+        enable = true;
+        authorizedKeysFiles = [ config.sops.secrets."ssh-public-key".path ];
+      };
     };
-  };
 
-  flake.darwinModules.ssh = { config, ... }: {
-    services.openssh = {
-      enable = true;
-      authorizedKeysFiles = [ config.sops.secrets."ssh-public-key".path ];
+    darwinModules.ssh = { config, ... }: {
+      services.openssh = {
+        enable = true;
+        authorizedKeysFiles = [ config.sops.secrets."ssh-public-key".path ];
+      };
     };
-  };
 
-  flake.homeManagerModules.ssh = { pkgs, config, ... }:
-   let
-    # Platform-specific 1Password agent socket path
-    onePasswordAgent =
-      if pkgs.stdenv.isLinux then
-        "~/.1password/agent.sock"
-      else
-        ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
-  in
-   {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      includes = [ "${config.sops.secrets."ssh-extra-config".path}" ];
-      matchBlocks."*" = {
-        identityAgent = onePasswordAgent;
+    homeManagerModules.ssh = { pkgs, config, ... }:
+    let
+      # Platform-specific 1Password agent socket path
+      onePasswordAgent =
+        if pkgs.stdenv.isLinux then
+          "~/.1password/agent.sock"
+        else
+          ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+    in
+    {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        includes = [ "${config.sops.secrets."ssh-extra-config".path}" ];
+        matchBlocks."*" = {
+          identityAgent = onePasswordAgent;
+        };
       };
     };
   };
