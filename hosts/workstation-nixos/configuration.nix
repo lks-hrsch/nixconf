f01075bd6a1de216c9a3587aa879e6dcfa053078
lks-hrsch <lkshrsch2001@outlook.com>
2026-02-15 14:49:30 +0100

    feat: fix permissions in stacks

--- hosts/workstation-nixos/configuration.nix
+++ hosts/workstation-nixos/configuration.nix
@@ -176,12 +176,6 @@ in
     };
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
