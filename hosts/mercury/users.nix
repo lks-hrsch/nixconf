b479ae4f12ba0c0aacfb348d8a8e5f193edf12cc
lks-hrsch <lkshrsch2001@outlook.com>
2026-03-26 11:48:21 +0100

    feat: refactor sops

--- hosts/mercury/users.nix
+++ hosts/mercury/users.nix
@@ -2,6 +2,14 @@ _: {
   configurations.nixos."mercury".module =
     { config, ... }:
     {
+      sops.secrets."root-password-hash" = {
+        sopsFile = ../../secrets/secrets-mercury.yaml;
+        neededForUsers = true;
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+
       users.users.root.hashedPasswordFile = config.sops.secrets."root-password-hash".path;
     };
 }
--- hosts/workstation-nixos/_home-nas-mounts.nix
+++ hosts/workstation-nixos/_home-nas-mounts.nix
@@ -8,6 +8,10 @@ let
   automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,x-systemd.requires=network-online.target,x-systemd.after=network-online.target";
 in
 {
+  sops.secrets."smb-credentials-mars" = {
+    owner = config.flake.users.owner.username;
+  };
+
   services.gvfs.enable = true;
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [
