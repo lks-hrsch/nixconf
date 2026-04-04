90abaff4672ebbb5448447c4a89de5e0099b0a41
lks-hrsch <lkshrsch2001@outlook.com>
2026-04-04 18:50:44 +0200

    fix: uncomment opportunity rebuild command and ensure newline at end of file

--- rebuild-all-mars-instances.sh
+++ rebuild-all-mars-instances.sh
@@ -3,3 +3,5 @@ nixos-rebuild switch --flake .#deimos --target-host root@192.168.1.13
 
 # nixos-rebuild switch --flake .#curiosity --target-host root@192.168.1.64
 # nixos-rebuild switch --flake .#opportunity --target-host root@192.168.1.74
+
+nixos-rebuild switch --flake .#mercury --target-host root@mercury.netbird.lukashirsch.de
