b9ca56d0cc35e4e95543bb4e36dcf046eae51619
lks-hrsch <lkshrsch2001@outlook.com>
2026-09-15 20:54:19 +0200

    fix: remove unused Cachix key for cuda-maintainers

--- modules/desktop/base.nix
+++ modules/desktop/base.nix
@@ -62,13 +62,11 @@
             "https://hyprland.cachix.org"
             "https://noctalia.cachix.org"
             "https://cache.nixos-cuda.org"
-            "https://cuda-maintainers.cachix.org"
           ];
           trusted-public-keys = [
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
             "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
             "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
           ];
         };
 
